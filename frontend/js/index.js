16c80761fe6c7368d0ae4ac04c0ff79f1ebaf6d3
Calin Crisan <ccrisan@gmail.com>
2019-10-23 22:24:57 +0300

    Frontend: rename notifications module to events

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -15,8 +15,8 @@ import * as Auth          from '$app/auth.js'
 import * as Cache         from '$app/cache.js'
 import DashboardSection   from '$app/dashboard/dashboard-section.js'
 import DevicesSection     from '$app/devices/devices-section.js'
+import * as Events        from '$app/events.js'
 import LoginSection       from '$app/login/login-section.js'
-import * as Notifications from '$app/notifications.js'
 import PortsSection       from '$app/ports/ports-section.js'
 import SettingsSection    from '$app/settings/settings-section.js'
 
@@ -119,7 +119,7 @@ function main() {
     API.addAccessLevelChangeListener(handleAccessLevelChange)
     API.addEventListener(handleAPIEvent)
 
-    Notifications.init()
+    Events.init()
 
     Auth.init()
     Auth.whenFinalAccessLevelReady.then(level => API.startListening())
