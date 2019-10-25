cb9de4af30ed27962eb7b3080892e832c825bc5f
Calin Crisan <ccrisan@gmail.com>
2019-10-25 21:31:19 +0300

    UI/API: update prefs api func names

--- qtoggleserver/web/handlers.py
+++ qtoggleserver/web/handlers.py
@@ -418,7 +418,7 @@ class DashboardPanelsHandler(APIHandler):
 
 class PrefsHandler(APIHandler):
     async def get(self):
-        await self.call_api_func(ui_api_funcs.get)
+        await self.call_api_func(ui_api_funcs.get_prefs)
 
     async def put(self):
-        await self.call_api_func(ui_api_funcs.put, default_status=204)
+        await self.call_api_func(ui_api_funcs.put_prefs, default_status=204)
