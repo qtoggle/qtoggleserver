268ba5ba584ebeb1d02fa46441d23aab5318179e
Calin Crisan <ccrisan@gmail.com>
2019-12-20 21:53:46 +0200

    Frontend/Dashboard: add MultiValueSlider

---
+++ frontend/js/dashboard/widgets/all-widgets/sliders-knobs/multi-value-slider.js
@@ -0,0 +1,40 @@
+
+import $ from '$qui/lib/jquery.module.js'
+
+import {gettext} from '$qui/base/i18n.js'
+import StockIcon from '$qui/icons/stock-icon.js'
+
+import * as Widgets from '$app/dashboard/widgets/widgets.js'
+
+import {ConfigForm as AnalogWidgetConfigForm} from '../analog-widget.js'
+import {AnalogWidget}                         from '../analog-widget.js'
+
+
+class ConfigForm extends AnalogWidgetConfigForm {
+
+    constructor(widget) {
+        super(widget, {ticksonly: true, tickColors: true})
+    }
+
+}
+
+
+export default class MultiValueSlider extends AnalogWidget {
+
+    constructor() {
+        super({ticksonly: true, tickColors: true})
+    }
+
+}
+
+// TODO es7 class fields
+MultiValueSlider.category = gettext('Sliders/Knobs')
+MultiValueSlider.displayName = gettext('Multi-value Slider')
+MultiValueSlider.typeName = 'MultiValueSlider'
+MultiValueSlider.icon = new StockIcon({name: 'widget', stockName: 'qtoggle'})
+MultiValueSlider.ConfigForm = ConfigForm
+MultiValueSlider.hResizable = true
+MultiValueSlider.vResizable = true
+
+
+Widgets.register(MultiValueSlider)
