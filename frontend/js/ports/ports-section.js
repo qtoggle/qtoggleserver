dffa3b32f1e66bef6ec517438c319746266761bc
Calin Crisan <ccrisan@gmail.com>
2020-01-16 19:53:02 +0200

    Frontend/Ports/PortForm: remove background changed warning for port value

--- frontend/js/ports/ports-section.js
+++ frontend/js/ports/ports-section.js
@@ -156,12 +156,6 @@ export default class PortsSection extends Section {
                     break
                 }
 
-                if (event.expected) {
-                    if (portForm && (portForm.getPortId() === event.params.id) && portForm.isWaitingValueChanged()) {
-                        portForm.clearWaitingValueChanged()
-                    }
-                }
-
                 /* Update port form */
                 if (portForm && (portForm.getPortId() === event.params.id) && (event.params.value != null)) {
                     let lastSync = Math.round(new Date().getTime() / 1000)
@@ -172,14 +166,7 @@ export default class PortsSection extends Section {
                         data['attr_last_sync'] = API.STD_PORT_ATTRDEFS['last_sync'].valueToUI(lastSync)
                     }
                     if (valueField && (valueField.getValue() !== event.params.value)) {
-                        if (valueField.isReadonly() || valueField.getValue() == null || event.expected) {
-                            data['value'] = event.params.value
-                        }
-                        else {
-                            if (!valueField.hasWarning() && !valueField.hasError() && !event.expected) {
-                                valueField.setWarning(gettext('Value has been updated in the meantime.'))
-                            }
-                        }
+                        data['value'] = event.params.value
                     }
 
                     if (Object.keys(data).length) {
