060859852e3a1c5474b2b995c6e7be8bab93bd38
Calin Crisan <ccrisan@gmail.com>
2020-02-28 19:33:19 +0200

    Jinja2: try debug frontend dir first when looking up templates

--- qtoggleserver/web/j2template.py
+++ qtoggleserver/web/j2template.py
@@ -47,7 +47,7 @@ def get_env() -> Environment:
 
     if _env is None:
         logger.debug('creating Jinja2 template environment')
-        loader = NamespaceLoader('qtoggleserver', [f'{FRONTEND_DIR}/html', f'{FRONTEND_DIR_DEBUG}/html'])
+        loader = NamespaceLoader('qtoggleserver', [f'{FRONTEND_DIR_DEBUG}/html', f'{FRONTEND_DIR}/html'])
         _env = Environment(loader=loader, autoescape=select_autoescape())
 
     return _env
