513dfa0a6df5fda3a89ad2703fdf62291bb85597
Calin Crisan <ccrisan@gmail.com>
2020-03-22 23:32:08 +0200

    Core/Expressions: add SEQUENCE function

--- qtoggleserver/core/expressions/various.py
+++ qtoggleserver/core/expressions/various.py
@@ -1,8 +1,11 @@
 
-from typing import Optional
+import time
+
+from typing import Optional, Set
 
 from qtoggleserver.core.typing import PortValue as CorePortValue
 
+from .exceptions import EvalSkipped
 from .functions import function, Function
 
 
@@ -46,3 +49,49 @@ class HystFunction(Function):
                                 (self._last_result != 0 and value >= threshold1))
 
         return self._last_result
+
+
+@function('SEQUENCE')
+class SequenceFunction(Function):
+    MIN_ARGS = 2
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self._last_time: float = 0
+
+    def get_deps(self) -> Set[str]:
+        return {'time_ms'}
+
+    def eval(self) -> CorePortValue:
+        now = time.time() * 1000
+
+        if self._last_time > 0:
+            self._last_time = now
+            raise EvalSkipped()
+
+        args = self.eval_args()
+        num_values = len(args) // 2
+        values = []
+        delays = []
+        total_delay = 0
+        for i in range(num_values):
+            values.append(args[i * 2])
+            delays.append(args[i * 2 + 1])
+            total_delay += args[i * 2 + 1]
+
+        if len(delays) < len(values):
+            delays.append(0)
+
+        delta = now - self._last_time
+        delta = delta % total_delay  # Work modulo total_delay, to create repeat effect
+        delay_so_far = 0
+        result = values[0]
+        for i in range(num_values):
+            delay_so_far += delays[i]
+            if delay_so_far >= delta:
+                result = values[i]
+                break
+
+        return result
+
