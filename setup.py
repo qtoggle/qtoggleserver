199be6631ddf49b5ead69080edbad324f32f9ba0
Calin Crisan <ccrisan@gmail.com>
2020-06-02 01:40:33 +0300

    Update qui to 1.11.0-alpha.5

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'pyhocon==0.3.54',
         'pyjwt==1.7.1',
         'pytz==2019.3',
-        'qui-server==1.11.0-alpha.4',
+        'qui-server==1.11.0-alpha.5',
         'tornado==6.0.4',
     ],
 
