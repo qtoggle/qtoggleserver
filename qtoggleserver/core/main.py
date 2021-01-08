359e1e39a708d9c624c23ce27f991552d7ab4e06
Calin Crisan <ccrisan@gmail.com>
2021-01-08 18:47:39 +0200

    core/main: Fix expression evaluation loop over ports

--- qtoggleserver/core/main.py
+++ qtoggleserver/core/main.py
@@ -141,7 +141,7 @@ async def handle_value_changes(
             await port.save()
 
     # Reevaluate the expressions depending on changed ports
-    for port in core_ports.get_all():
+    for port in list(core_ports.get_all()):
         if not port.is_enabled():
             continue
 
