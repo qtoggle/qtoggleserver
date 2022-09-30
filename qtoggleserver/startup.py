9dbbc6b91d96c85c64b537e62a8eaa2490d88801
Calin Crisan <ccrisan@gmail.com>
2022-09-30 12:15:37 +0300

    Fix import order issue

--- qtoggleserver/startup.py
+++ qtoggleserver/startup.py
@@ -10,13 +10,16 @@ from typing import Any, Optional
 
 from tornado import httpclient, netutil
 
-from qtoggleserver import peripherals  # this must be imported after core.ports
 from qtoggleserver import persist, slaves, system, version, web
 from qtoggleserver.conf import settings
 from qtoggleserver.core import device, events, history, main, ports, reverse, sessions, vports, webhooks
 from qtoggleserver.slaves import devices as slaves_devices
 from qtoggleserver.utils import conf as conf_utils
 from qtoggleserver.utils import logging as logging_utils
+
+
+# Following import must be imported after `core.ports`
+from qtoggleserver import peripherals  # isort: split
 
 
 logger: Optional[logging.Logger] = None
