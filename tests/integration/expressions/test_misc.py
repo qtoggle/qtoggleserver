7d66cb151c6ced6552f82261d7990d6f63b2317b
Calin Crisan <ccrisan@gmail.com>
2025-12-01 11:40:07 +0200

    core/expressions: Add more tests for port values

--- tests/integration/expressions/test_misc.py
+++ tests/integration/expressions/test_misc.py
@@ -19,3 +19,25 @@ async def test_expression_port_write_only_changed(mock_num_port1, mocker):
     await main.update()
     await asyncio.sleep(0.1)
     mock_num_port1.transform_and_write_value.assert_not_called()
+
+
+async def test_expression_port_self_value(mock_num_port1):
+    """Test that a port can reference its own value using `$`."""
+
+    mock_num_port1.set_writable(True)
+    mock_num_port1.set_last_read_value(15)
+    await mock_num_port1.set_attr("expression", "ADD($, 1)")
+    mock_num_port1.set_last_read_value(25)
+    await asyncio.sleep(0.1)
+    assert mock_num_port1.get_last_written_value() == 16
+
+
+async def test_expression_port_own_value(mock_num_port1):
+    """Test that a port can reference its own value using its id."""
+
+    mock_num_port1.set_writable(True)
+    mock_num_port1.set_last_read_value(15)
+    await mock_num_port1.set_attr("expression", "ADD($nid1, 1)")
+    mock_num_port1.set_last_read_value(25)
+    await asyncio.sleep(0.1)
+    assert mock_num_port1.get_last_written_value() == 16
--- tests/unit/qtoggleserver/core/expressions/test_port.py
+++ tests/unit/qtoggleserver/core/expressions/test_port.py
@@ -1,7 +1,8 @@
-import asyncio
+import pytest
 
 from qtoggleserver.core.expressions import Role, parse
-from qtoggleserver.core.expressions.ports import PortRef, PortValue
+from qtoggleserver.core.expressions.exceptions import DisabledPort, PortValueUnavailable, UnknownPortId
+from qtoggleserver.core.expressions.ports import PortRef, PortValue, SelfPortRef, SelfPortValue
 
 
 class TestPortValue:
@@ -11,48 +12,83 @@ class TestPortValue:
         assert e.port_id == "nid2"
         assert e.get_port() == mock_num_port2
 
-    def test_self_parse(self, mock_num_port1):
+    def test_parse_self(self, mock_num_port1):
         e = parse("nid1", "$", role=Role.VALUE)
-        assert isinstance(e, PortValue)
+        assert isinstance(e, SelfPortValue)
         assert e.port_id == "nid1"
         assert e.get_port() == mock_num_port1
 
-    def test_inexistent(self, mock_num_port1):
+    def test_parse_inexistent(self, mock_num_port1):
         e = parse("nid1", "$nid2", role=Role.VALUE)
         assert isinstance(e, PortValue)
         assert e.port_id == "nid2"
         assert e.get_port() is None
 
-    async def test_self_value(self, mock_num_port1):
-        mock_num_port1.set_writable(True)
-        mock_num_port1.set_last_read_value(15)
-        await mock_num_port1.set_attr("expression", "ADD($, 1)")
-        mock_num_port1.set_last_read_value(25)
-        await asyncio.sleep(0.1)
-        assert mock_num_port1.get_last_written_value() == 16
+    async def test_eval(self, mock_num_port1, dummy_eval_context):
+        """Should return the port value from supplied context."""
 
-    async def test_own_value(self, mock_num_port1):
-        mock_num_port1.set_writable(True)
-        mock_num_port1.set_last_read_value(15)
-        await mock_num_port1.set_attr("expression", "ADD($nid1, 1)")
-        mock_num_port1.set_last_read_value(25)
-        await asyncio.sleep(0.1)
-        assert mock_num_port1.get_last_written_value() == 16
+        e = PortValue("nid1", prefix="$", role=Role.VALUE)
+        dummy_eval_context.port_values["nid1"] = 16
+        assert await e._eval(dummy_eval_context) == 16
 
-    async def test_allow_self_dependency(self, mock_num_port1):
-        mock_num_port1.set_writable(True)
-        await mock_num_port1.set_attr("expression", "ADD($nid1, 10)")
+    async def test_eval_unknown(self, mock_num_port1, dummy_eval_context):
+        """Should raise UnknownPortId when the referenced port doesn't exist."""
+
+        e = PortValue("inexistent", prefix="$", role=Role.VALUE)
+        with pytest.raises(UnknownPortId) as exc_info:
+            await e._eval(dummy_eval_context)
+        assert exc_info.value.port_id == "inexistent"
+
+    async def test_eval_unavailable(self, mock_num_port1, dummy_eval_context):
+        """Should raise PortValueUnavailable when the referenced port's value is unavailable."""
+
+        e = PortValue("nid1", prefix="$", role=Role.VALUE)
+        dummy_eval_context.port_values["nid1"] = None
+        with pytest.raises(PortValueUnavailable) as exc_info:
+            await e._eval(dummy_eval_context)
+        assert exc_info.value.port_id == "nid1"
+
+    async def test_eval_disabled(self, mock_num_port1, dummy_eval_context, mocker):
+        """Should raise DisabledPort when the referenced port is disabled."""
+
+        e = PortValue("nid1", prefix="$", role=Role.VALUE)
+        dummy_eval_context.port_values["nid1"] = None
+        mocker.patch.object(mock_num_port1, "is_enabled", return_value=False)
+
+        with pytest.raises(DisabledPort) as exc_info:
+            await e._eval(dummy_eval_context)
+        assert exc_info.value.port_id == "nid1"
 
 
 class TestPortRef:
-    def test_port_ref_parse(self, mock_num_port1, mock_num_port2):
+    def test_parse(self, mock_num_port1, mock_num_port2):
         e = parse("nid1", "@nid2", role=Role.VALUE)
         assert isinstance(e, PortRef)
         assert e.port_id == "nid2"
         assert e.get_port() == mock_num_port2
 
-    def test_port_ref_inexistent(self, mock_num_port1):
+    def test_parse_self(self, mock_num_port1):
+        e = parse("nid1", "@", role=Role.VALUE)
+        assert isinstance(e, SelfPortRef)
+        assert e.port_id == "nid1"
+        assert e.get_port() == mock_num_port1
+
+    def test_parse_inexistent(self, mock_num_port1):
         e = parse("nid1", "@nid2", role=Role.VALUE)
         assert isinstance(e, PortRef)
         assert e.port_id == "nid2"
         assert e.get_port() is None
+
+    async def test_eval(self, mock_num_port1, dummy_eval_context):
+        """Should return the port value from supplied context."""
+
+        e = PortRef("nid1", prefix="@", role=Role.VALUE)
+        assert await e._eval(dummy_eval_context) == "nid1"
+
+    async def test_eval_unknown(self, mock_num_port1, dummy_eval_context):
+        """Should raise UnknownPortId when the referenced port doesn't exist."""
+
+        e = PortRef("inexistent", prefix="@", role=Role.VALUE)
+        with pytest.raises(UnknownPortId) as exc_info:
+            await e._eval(dummy_eval_context)
+        assert exc_info.value.port_id == "inexistent"
