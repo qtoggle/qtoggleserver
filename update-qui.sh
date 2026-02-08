c7f1c53d215d7eb8cc71dcdc6f0a4ef9791723c5
Calin Crisan <ccrisan@gmail.com>
2026-02-08 12:45:46 +0200

    frontend: Update qui to 1.19.2

--- update-qui.sh
+++ update-qui.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+set -e
+
 if [ -z "$1" ]; then
     echo "missing argument version"
     exit 1
 fi
 
-cd $(dirname $0)/qtoggleserver/frontend
+cd "$(dirname $0)/qtoggleserver/frontend"
 npm cache clean --force
 npm install @qtoggle/qui@$1
 git checkout package.json
